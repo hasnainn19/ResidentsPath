45d2c511554fecfdd772622f2eb72bcbbf5dc9fe
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2026-02-06 21:33:45 +0000

    Create placeolder reference&qrcode page

---
+++ src/ReferencePage.tsx
@@ -0,0 +1,13 @@
+// import { useState } from "react";
+
+export default function ReferencePage() {
+
+
+
+return (
+    <div className="reference-page">
+        <h1>Reference & QR Code Page</h1>
+    </div>
+)
+
+}
