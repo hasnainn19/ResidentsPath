aa01a796c86521d12988637255ae92e4bbf895cd
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2026-02-06 23:24:28 +0000

    Resize boxes and improve placement of components

--- src/ReferencePage.tsx
+++ src/ReferencePage.tsx
@@ -11,13 +11,14 @@ import {
 import SearchRoundedIcon from '@mui/icons-material/SearchRounded';
 import QrCodeScannerRoundedIcon from '@mui/icons-material/QrCodeScannerRounded';
 import EastRoundedIcon from '@mui/icons-material/EastRounded';
+import QrCode2OutlinedIcon from '@mui/icons-material/QrCode2Outlined';
 
 export default function ReferencePage() {
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: '#fff',
   ...theme.typography.body2,
-  padding: theme.spacing(1),
+  padding: theme.spacing(3),
   textAlign: 'center',
   color: (theme.vars ?? theme).palette.text.secondary,
   ...theme.applyStyles('dark', {
@@ -48,29 +49,48 @@ const ScanBox = styled(Box)(({ theme }) => ({
 return (
     <div className="reference-page">
         <h1>Use one of the following methods to view your queue details</h1>
-        <Grid container spacing={3}>
-            <Grid size="grow">
-                <Item>
+        <Grid container 
+            spacing={3} 
+            direction="row"
+            alignItems="stretch"
+            sx={{ justifyContent: "center", }}
+        >
+            <Grid sx={{ display: 'flex'}} size={4}>
+                <Item sx={{ display: 'flex', flexDirection: 'column', flex: 1, height: '100%' }}>
                     <SearchRoundedIcon />
                     <h3>Manual Entry</h3>
                     <h4>Enter your reference code here:</h4>
-                    <TextField id="outlined-basic" variant="outlined" fullWidth />
-                    <Button variant="contained" endIcon={<EastRoundedIcon />} sx={{ mt: 2, width: '100%' }}>
-                        Check Status
-                    </Button>
+                    <Box sx={{ mt: 'auto', width: '100%', maxWidth: 480, mx: 'auto' }}>
+                        <TextField
+                            id="outlined-basic"
+                            variant="outlined"
+                            fullWidth
+                            placeholder="Reference code"
+                            sx={{ mb: 2 }}
+                        />
+                        <Button
+                            variant="contained"
+                            endIcon={<EastRoundedIcon />}
+                            sx={{ width: '100%' }}
+                        >
+                            Check Status
+                        </Button>
+                    </Box>
                 </Item>
             </Grid>
 
-            <Grid size="grow">
-                <Item>
-                    <QrCodeScannerRoundedIcon />
+            <Grid sx={{ display: 'flex'}} size={4}>
+                <Item sx={{ display: 'flex', flexDirection: 'column', flex: 1, height: '100%' }}>
+                    <QrCode2OutlinedIcon />
                     <h3>Scan QR Code</h3>
                     <h4>Click to use the camera to scan the QR code:</h4>
 
-                    <ScanBox onClick={() => console.log('Start camera')}>
-                    <QrCodeScannerRoundedIcon fontSize="large" />
-                    <Box mt={1}>Tap to start camera</Box>
-                    </ScanBox>
+                    <Box sx={{ mt: 'auto', width: '100%', maxWidth: 480, mx: 'auto' }}>
+                        <ScanBox onClick={() => console.log('Start camera')} sx={{ width: '100%' }}>
+                            <QrCodeScannerRoundedIcon fontSize="large" />
+                            <Box mt={1}>Tap to start camera</Box>
+                        </ScanBox>
+                    </Box>
                 </Item>
             </Grid>
 
