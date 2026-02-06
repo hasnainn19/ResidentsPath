b46de733aee473f8551763412b56fa8c249f834b
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2026-02-06 21:55:44 +0000

    Create boxes for Manual Entry and QR Code

--- src/ReferencePage.tsx
+++ src/ReferencePage.tsx
@@ -1,12 +1,36 @@
-// import { useState } from "react";
+import { Grid } from '@mui/material';
+import { styled } from '@mui/material/styles';
+import Paper from '@mui/material/Paper';
 
 export default function ReferencePage() {
 
-
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: (theme.vars ?? theme).palette.text.secondary,
+  ...theme.applyStyles('dark', {
+    backgroundColor: '#1A2027',
+  }),
+}));
 
 return (
     <div className="reference-page">
         <h1>Reference & QR Code Page</h1>
+        <Grid container spacing={3}>
+            <Grid size="grow">
+                <Item>
+                    Manual Entry
+                </Item>
+            </Grid>
+
+            <Grid size="grow">
+                <Item>
+                    Scan QR Code
+                </Item>
+            </Grid>
+        </Grid>
     </div>
 )
 
