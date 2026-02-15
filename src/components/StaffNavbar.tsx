5d4f4a5efdda188418823444042881abaadfdc2b
Abu-Bakarr Jalloh <k22010427@kcl.ac.uk>
2026-02-15 20:52:24 +0000

    Add profile section to Navbar

--- src/components/StaffNavbar.tsx
+++ src/components/StaffNavbar.tsx
@@ -9,6 +9,9 @@ import {
   Box,
   styled,
   Paper,
+  Avatar,
+  Accordion,
+  AccordionSummary,
 } from "@mui/material";
 
 import DashboardIcon from "@mui/icons-material/Dashboard";
@@ -51,29 +54,103 @@ const StaffNavbar = () => {
         </Box>
       </Toolbar>
 
-      <List sx={{ mt: 3 }}>
-        <StaffNavItem icon={<DashboardIcon />} label="Dashboard" url="/staff" />
-        <StaffNavItem
-          icon={<QueueIcon />}
-          label="Current Cases"
-          url="/staff/cases"
-        />
-        {/* <StaffNavItem
-          icon={<EventIcon />}
-          label="Appointments"
-          url="/staff/appointments"
-        /> */}
-        <StaffNavItem
-          icon={<EventIcon />}
-          label="Current Staff"
-          url="/staff/directory"
-        />
-        <StaffNavItem
-          icon={<WorkIcon />}
-          label="My cases"
-          url="/staff/my-cases"
-        />
-        <StaffNavItem icon={<SettingsIcon />} label="Settings" url="/staff" />
+      <List
+        sx={{
+          mt: 3,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          height: "calc(100% - 64px)",
+        }}
+      >
+        <Box>
+          <StaffNavItem
+            icon={<DashboardIcon />}
+            label="Dashboard"
+            url="/staff"
+          />
+          <StaffNavItem
+            icon={<QueueIcon />}
+            label="Current Cases"
+            url="/staff/cases"
+          />
+          {/* <StaffNavItem
+            icon={<EventIcon />}
+            label="Appointments"
+            url="/staff/appointments"
+          /> */}
+          <StaffNavItem
+            icon={<EventIcon />}
+            label="Current Staff"
+            url="/staff/directory"
+          />
+          <StaffNavItem
+            icon={<WorkIcon />}
+            label="My cases"
+            url="/staff/my-cases"
+          />
+          <StaffNavItem icon={<SettingsIcon />} label="Settings" url="/staff" />
+        </Box>
+        <Box sx={{}}>
+          <Accordion
+            sx={{
+              backgroundColor: "transparent",
+              color: "white",
+              boxShadow: "none",
+            }}
+          >
+            <AccordionSummary
+              sx={{
+                px: 0,
+                py: 0,
+                margin: 0,
+                display: "flex",
+                alignItems: "center",
+                gap: 2,
+              }}
+            >
+              <Avatar
+                src={"https://i.pravatar.cc/150?img=5"}
+                sx={{
+                  width: 50,
+                  height: 50,
+                  boxShadow: "0 2px 8px rgba(0,0,0,0.1)",
+                }}
+              />
+              <Box sx={{ ml: 2 }}>
+                <Typography variant="body1" fontWeight={500} sx={{ mt: 1 }}>
+                  Emily Rodriguez
+                </Typography>
+                <Typography color="text.primary">Council Services</Typography>
+              </Box>
+            </AccordionSummary>
+            <Box sx={{ backgroundColor: "white", padding: 0, width: "100%" }}>
+              <List>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{ mt: 0.5, fontStyle: "italic" }}
+                >
+                  View Profile
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{ mt: 0.5, fontStyle: "italic" }}
+                >
+                  View Audit Logs
+                </Typography>
+                <Typography
+                  variant="body2"
+                  color="text.secondary"
+                  sx={{ mt: 0.5, fontStyle: "italic" }}
+                >
+                  Logout
+                </Typography>
+              </List>
+            </Box>
+          </Accordion>
+        </Box>
       </List>
     </Drawer>
   );
