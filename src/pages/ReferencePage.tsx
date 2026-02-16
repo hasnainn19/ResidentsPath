b57676eee835fbb3e8a353912e8a46215b935c5a
Naomi Quartsin <naomi.quartsin@kcl.ac.uk>
2026-02-16 22:16:35 +0000

    Remove pixel css and fix page

--- src/pages/ReferencePage.tsx
+++ src/pages/ReferencePage.tsx
@@ -14,8 +14,8 @@ import TextToSpeechButton from "../components/TextToSpeechButton";
 
 const ReferencePage = () => {
     const scannerRef = useRef<Html5Qrcode | null>(null);
-    const [scanning, setScanning] = useState(false);
-    const [refNo, setRefNo] = useState('');
+    const [ scanning, setScanning ] = useState(false);
+    const [ refNo, setRefNo ] = useState('');
     const [ qrScanError, setQrScanError] = useState('');
 
 
@@ -23,8 +23,7 @@ const ReferencePage = () => {
     const ScanButton = styled(ButtonBase)(({ theme }) => ({
         display: 'flex',
         width: '100%',
-        height: 220,
-        // position: 'relative', 
+        height: '100%',
         backgroundColor: theme.palette.secondary.light,
         border: `2px dashed ${grey[600]}`,
         borderRadius: 8,
@@ -37,61 +36,42 @@ const ReferencePage = () => {
         }
     }));
 
-    // function handleQRScanner() {
-    //     setScanning(true);
-    //     setTimeout(() => {
-    //         const scanner = new Html5Qrcode("qr-reader");
-    //         scannerRef.current = scanner;
-    //         scanner.start(
-    //         { facingMode: "environment" }, // rear camera
-    //         { fps: 10, qrbox: 250 },
-    //         (decodedText) => {
-    //             setRefNo(decodedText)
-    //             console.log("Decoded QR code:", decodedText);
-    //             // process ref no.
-    //             stopScanner();
-    //         },
-    //         (error) => {
-    //             setQrScanError(error);
-    //         }
-    //         );
-    // }, 300)};
-function handleQRScanner() {
-    if (scanning) return;
-    setScanning(true);
-}
+    function handleQRScanner() {
+        if (scanning) return;
+        setScanning(true);
+    }
 
 
-useEffect(() => {
-    if (!scanning) return;
-    if (scannerRef.current) return;
+    useEffect(() => {
+        if (!scanning) return;
+        if (scannerRef.current) return;
 
-    let cancelled = false;
+        let cancelled = false;
 
-    requestAnimationFrame(() => {
-        if (cancelled) return;
+        requestAnimationFrame(() => {
+            if (cancelled) return;
 
-        const scanner = new Html5Qrcode("qr-reader");
-        scannerRef.current = scanner;
+            const scanner = new Html5Qrcode("qr-reader");
+            scannerRef.current = scanner;
 
-        scanner.start(
-            { facingMode: "environment" },
-            { fps: 10, qrbox: 250 },
-            (decodedText) => {
-                setRefNo(decodedText);
-                console.log("Decoded QR code:", decodedText);
-                stopScanner();
-            },
-            (error) => {
-                setQrScanError(error);
-            }
-        );
-    });
+            scanner.start(
+                { facingMode: "environment" },
+                { fps: 10, qrbox: 250 },
+                (decodedText) => {
+                    setRefNo(decodedText);
+                    console.log("Decoded QR code:", decodedText);
+                    stopScanner();
+                },
+                (error) => {
+                    setQrScanError(error);
+                }
+            );
+        });
 
-    return () => {
-        cancelled = true;
-    };
-}, [scanning]);
+        return () => {
+            cancelled = true;
+        };
+    }, [scanning]);
 
 
 
@@ -114,7 +94,7 @@ useEffect(() => {
     return (
     <>
         <Navbar />
-        <Container maxWidth="lg" sx={{ py: 6, textAlign: 'center'  }}>
+        <Container maxWidth="lg"  sx={{ py: 6, textAlign: 'center', height:'85vh'  }}>
             <Typography variant="h3" component="h1"  gutterBottom sx={{ fontWeight: 700 , mb: 6 }}>
                 Use one of the following methods to see more details
             </Typography>
@@ -122,10 +102,10 @@ useEffect(() => {
                 Check your queue details OR check in for an appointment
                 <TextToSpeechButton text='Use one of the following methods to either check yout queue details or check in for an appointment'/>
             </Typography>
-            <Grid container spacing={3} sx={{ justifyContent: "center", }}>
-                <Grid sx={{ display: 'flex'}} size={6}>
+            <Grid container spacing={3} sx={{ justifyContent: "center", height: '80%' }}>
+                <Grid sx={{ display: 'flex', height: '100%'}} size={5.5}>
                     <Card sx={{ display: 'flex', flexDirection: 'column', flex: 1, borderRadius: 3, height: '100%' }}>
-                        <CardContent sx={{ flex: 1, display: 'flex', flexDirection: 'column', p: 4 }}>
+                        <CardContent sx={{ flex: 1, display: 'flex', flexDirection: 'column', p: 4}}>
                             <Avatar sx={{ bgcolor: 'primary.light', color: 'primary.dark' , mb: 2 }}>
                                 <SearchRoundedIcon />
                             </Avatar>
@@ -138,9 +118,9 @@ useEffect(() => {
                             <TextToSpeechButton text='For manual entry, enter your reference number in the text field and press the button to check your status.'/>
                         </CardContent>
 
-                        <CardActions sx={{ px: 4, pb: 4 }}>
+                        <CardActions sx={{ px: 4, pb: 4}}>
                             <Box sx={{ mt: 'auto', width: '100%' }}>
-                                <TextField fullWidth id="outlined-search" label="Reference code" sx={{ mb: 3 }} value={refNo} />
+                                <TextField fullWidth id="outlined-search" label="Reference code" sx={{ mb: 3 }} onChange={(e) => setRefNo(e.target.value)} />
                                 <Tooltip title="Check your status" placement="top">
                                     <Button variant="contained" onClick={handleCheckStatus} endIcon={<ManageSearchOutlinedIcon />} className='referencepage-check-status-btn' sx={{ backgroundColor: 'primary.dark', width: '100%' }}>
                                         Check Status
@@ -151,8 +131,8 @@ useEffect(() => {
                     </Card>
                 </Grid>
 
-                <Grid  sx={{ display: 'flex'}} size={6}>
-                    <Card sx={{ display: 'flex', flexDirection: 'column', flex: 1, height: '100%', borderRadius: 3 }}>
+                <Grid sx={{ display: 'flex', height: '100%' }} size={5.5}>
+                    <Card sx={{ display: 'flex', flexDirection: 'column', flex: 1,  borderRadius: 3 }}>
                         <CardContent sx={{ flex: 1, display: 'flex', flexDirection: 'column' , p: 4 }}>
                             <Avatar sx={{ bgcolor: 'primary.dark' , mb: 2 }}>
                             <QrCode2OutlinedIcon />
@@ -166,28 +146,34 @@ useEffect(() => {
                             <TextToSpeechButton text='For QR code entry, click the button below to use the camera to scan the QR code.'/>
                         </CardContent>
 
-                        <CardActions sx={{ px: 4, pb: 4, justifyContent: 'center' }}>
-                            <Box sx={{ width: '100%', mx: 'auto', position: 'relative' }}>
-                                    <ScanButton onClick={handleQRScanner} sx={{ flexDirection: 'column', py: 3 }}>
+                        <CardActions sx={{ px: 3, pb: 4, display: 'flex', flexDirection: 'column', justifyContent: 'center', flex: 1, height:'100%'}}>
+                            <Box sx={{  width: '100%', heigt: '100%', mx: 'auto', position: 'relative', flex: 1, display: 'flex', flexDirection: 'column'}}>
+                                <ScanButton onClick={handleQRScanner} sx={{ flexDirection: 'column', py: 3 }}>
                                     {!scanning && (
                                     <>
-                                        <QrCodeScannerRoundedIcon fontSize="large" />
-                                        <Box component="span" sx={{ mt: 2, fontWeight: 600 }}>
-                                        Tap to open scanner
+                                        <QrCodeScannerRoundedIcon fontSize="large" size="large" />
+                                        <Box component="span" sx={{ mt: 2, fontWeight: 600, fontSize:"large" }}>
+                                            Tap to open scanner
                                         </Box>
                                     </>
                                     )}
                                 </ScanButton>
-                                <Box sx={{ position: 'absolute', top: 0, left: 0, width: '100%', height: 220, zIndex: 10, visibility: scanning ? 'visible' : 'hidden', pointerEvents: scanning ? 'auto' : 'none', overflow: 'hidden', borderRadius: 1, }}>
-                                    <div id="qr-reader" style={{ width: '100%', height: '100%', maxWidth: '100%', maxHeight: '100%', transform: 'translateZ(0)', objectFit: 'cover' }} />
+                                <Box sx={{  position: 'absolute', top: 0, left: 0, width: '100%', height: '100%', zIndex: 10, visibility: scanning ? 'visible' : 'hidden', pointerEvents: scanning ? 'auto' : 'none', overflow: 'hidden', borderRadius: 1, }}>
+                                    <div id="qr-reader" style={{  width: '100%', height: '100%', maxWidth: '100%', maxHeight: '100%', transform: 'translateZ(0)', objectFit: 'cover' }} />
                                     {scanning && (
                                         <Tooltip title="Cancel QR Scan" placement="top">
-                                        <Button variant='contained' size="small" onClick={(e) => { e.stopPropagation(); stopScanner(); }} sx={{ position: 'absolute', top: 6, right: 6, zIndex: 20 }} >
-                                            Cancel
-                                        </Button>
+                                            <Button variant='contained' size="small" onClick={(e) => { e.stopPropagation(); stopScanner(); }} sx={{ position: 'absolute', top: '3%', right: '2%', zIndex: 20 }} >
+                                                Cancel
+                                            </Button>
                                         </Tooltip>
                                     )}
                                 </Box>
+                                {/* Placeholder showing qr code extracts text */}
+                                {refNo && (
+                                    <Typography variant="body2" color="text.primary" sx={{ mt: 1 }}>
+                                        Reference code retrieved: {refNo}
+                                    </Typography>
+                                )}
                             </Box>
                         </CardActions>
                     </Card>
