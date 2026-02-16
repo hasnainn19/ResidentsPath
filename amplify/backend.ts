f23291de1f977fe41f22c2eecd9d3bf3a0c27efc
Hasnain N <hasnainn3666@gmail.com>
2026-02-16 04:30:51 +0000

    Remove comments from amplify generated the project

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,7 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
-import { hello } from './functions/hello/resource';
 
 /**
  * @see https://docs.amplify.aws/react/build-a-backend/ to add storage, functions, and more
@@ -9,5 +8,4 @@ import { hello } from './functions/hello/resource';
 defineBackend({
   auth,
   data,
-  hello,
 });
