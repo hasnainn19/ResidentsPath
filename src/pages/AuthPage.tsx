ef66f5aef2bfbc0ba052a9e571a8aa83c1bd5a32
Hasnain N <hasnainn3666@gmail.com>
2026-02-17 17:55:03 +0000

    Add landing page and a placeholder auth page

---
+++ src/pages/AuthPage.tsx
@@ -0,0 +1,7 @@
+const AuthPage = () => {
+  return (
+	<h1>Sign Up or Log in</h1>
+  )
+}
+
+export default AuthPage
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,11 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import AuthPage from "./pages/AuthPage";
 import App from "./App";
 import ReferencePage from "./pages/ReferencePage";
 import BookingPage from "./pages/BookingPage";
 
 export const router = createBrowserRouter([
     {
-        path: "/", element: <App /> 
+        path: "/", element:  <LandingPage />
+    },
+    {
+        path: "/auth", element: <AuthPage />
+    },
+    {
+        path: "/start", element: <App />
     },
     {
         path: "/referencepage", element: <ReferencePage />
