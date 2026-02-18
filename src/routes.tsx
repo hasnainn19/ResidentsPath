880ed8607a571acf555222ca5bb449774e872939
Romina <k22034001@kcl.ac.uk>
2026-02-18 11:31:27 +0000

    Add node version to package file

--- src/routes.tsx
+++ src/routes.tsx
@@ -13,7 +13,7 @@ export const router = createBrowserRouter([
     },
     {
         path: "/bookingpage", element: <BookingPage />
-    }
+    },
     {
         path: "/userdashboard", element: <UserDashboard /> 
     },
