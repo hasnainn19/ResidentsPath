6e3ba0e4151309a54e50cd1f2ac6d2b578f1c705
Romina <k22034001@kcl.ac.uk>
2026-03-16 21:29:54 +0000

    Remove extra welcome from landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -28,9 +28,6 @@ export default function LandingPage() {
 							: `${translate("landing-welcome")}`
 						}
 					</Typography>
-					<Typography variant="h5" color="text.secondary" sx={{ mb: 4 }}>
-						{translate("landing-welcome-back")}
-					</Typography>
 				</Box>
 
 				{/* Cards vary based on authentication status */}
