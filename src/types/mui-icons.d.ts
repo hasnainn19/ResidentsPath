8cc2fdfb1716e22d28669b96fb8fd3fe5398382c
Romina <k22034001@kcl.ac.uk>
2026-03-23 14:44:13 +0000

    Add file to fix mui icons build error

---
+++ src/types/mui-icons.d.ts
@@ -0,0 +1,7 @@
+// src/types/mui-icons.d.ts
+declare module '@mui/icons-material/*' {
+  import { SvgIconProps } from '@mui/material/SvgIcon';
+  import React from 'react';
+  const content: React.ComponentType<SvgIconProps>;
+  export default content;
+}
